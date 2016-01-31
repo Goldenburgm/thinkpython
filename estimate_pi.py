459f472baa12718443220289986c5f23b434d78f
Goldenburgm <goldenburm@gmail.com>
2016-01-31 15:51:36 -0200

    Think Python's book projects

---
+++ estimate_pi.py
@@ -0,0 +1,15 @@
+import math
+
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
+			
+
+def estimate_py():
+	sum = 2 * math.sqrt(2) / 9801
+	k = 0
+#	while True
+
+print factorial(5)		
