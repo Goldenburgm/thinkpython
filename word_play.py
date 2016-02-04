928dbea51474ebdca6aefad9119e978ab54f907d
Goldenburgm <goldenburm@gmail.com>
2016-02-04 21:45:21 -0200

    New function finds all words in the file which have no char and calculatio the ratio of non-e words.

--- word_play.py
+++ word_play.py
@@ -1,12 +1,32 @@
+def has_no_e(word):
+	"""
+	Returns true if the given word has no e. 
+	"""			
+	for char in word:
+		if char == "e":
+			return False
+	return True	
+
 def read_words():
 	"""
 	Checks every line in file words.txt and prints
 	the ones that are over 20 characters
 	"""
-	fin = open("words.txt")
-	for line in fin:
+	file_words = open("words.txt")
+	lines = 0
+	no_e_count = 0
+	for line in file_words:
+		lines += 1
 		word = line.strip()
-		if len(word) > 20:
+		if has_no_e(word) == True:
 			print word
+			no_e_count += 1
+	percentage = (float(no_e_count) / float(lines)) * 100
+	print "%d percent of the words have no e!"	%(percentage)	
+	
+		
 
-read_words()			
+
+
+
+read_words()		
