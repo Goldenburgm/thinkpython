5bdc3abfdb495ec51b0aade5817221d24fca0a0a
Goldenburgm <goldenburm@gmail.com>
2016-02-09 18:00:18 -0200

    Function has_no_sub is now working. Changes have been made in the avoids() function, now it returns False if any of the characters in word2 are in word1.

--- word_play.py
+++ word_play.py
@@ -10,18 +10,19 @@ def has_no_e(word):
 def read_words():
 	"""
 	Checks every line in file words.txt and prints
-	the ones that are over 20 characters
+	the ones that have no e, and the percentage
+	of words without the e character
 	"""
-	file_words = open("words.txt")
-	lines = 0
-	no_e_count = 0
+	file_words = open("words.txt") #opening the file words.txt and assigning it to file_words
+	lines = 0 #starting lines count
+	no_e_count = 0 #starting lines without e count
 	for line in file_words:
-		lines += 1
-		word = line.strip()
+		lines += 1 #counts every line in the file
+		word = line.strip() #removes empty spaces from each line
 		if has_no_e(word) == True:
 			print word
-			no_e_count += 1
-	percentage = (float(no_e_count) / float(lines)) * 100
+			no_e_count += 1 #counts every line without e in the file
+	percentage = (float(no_e_count) / float(lines)) * 100 
 	print "%d percent of the words have no e!"	%(percentage)	
 	
 		
@@ -30,24 +31,31 @@ def avoids(word, f):
 	word: string
 	f: string
 
-	Receives a word and checks if it doesn't contain string
-	f. If it doesn't, it returns True. 
+	Receives a word and checks if it contains any character in
+	string f. If it doesn't, it returns True. 
 	"""
 
-	if f.upper() in word.upper():
-		return False
-	return True	
+	for char in f:
+		if char in word:
+			return False
+	return True			
+
 
 def has_no_sub(sub):
 	"""
 	sub: string
 
-	Checks how many words in words.txt have no string 
-	sub
-	"""
-	
+	Checks how many words in words.txt don't have any of the
+	characters in string sub, and prints them on the screen.
+	"""	
+	file_words = open("words.txt")
+	count = 0
+	for line in file_words:
+		word = line.strip()
+		if avoids(word, sub) == True:
+			print word
+			count += 1			
+	print "%s words don't have any of the characters in %s" %(count, sub)		
 
 
-
-
-print avoids("Python", "pyti")
+has_no_sub("abcdefghijklmno")
