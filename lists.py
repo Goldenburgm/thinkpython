d7804e168541336bc50925eb6cbd14e7ba5e7653
Suporte <goldenburgm@gmail.com>
2016-02-29 13:07:55 -0300

    New middle() and chop() functions.

--- lists.py
+++ lists.py
@@ -1,11 +1,12 @@
 nested_list = [1, [1, 1, 1], [1, 1, 1, 3]]
 nested_string_list = ["a", ["a", "b", "c"], "d"]
 cumulative = [1, [1, 1], 3]
+t = [1, 3, 5, 7, 8]
 
 def nested_sum(l):
 	"""
 	Receives list l, and sums all its elements, including
-	nested lists.
+	nested lists. Returns: integer.
 	"""
 	total = 0
 	for x in l:
@@ -18,7 +19,8 @@ def nested_sum(l):
 def capitalize_all(l):
 	"""
 	Receives list l and for each element that is a String,
-	it will return a capitalized version of it.
+	it will return a capitalized version of it. Returns:
+	new list.
 	"""	
 	result = []
 	for s in l:
@@ -29,7 +31,7 @@ def capitalize_all(l):
 def cumulative_sum(l):
 	"""
 	Receives a list and return its cumulative sum (works
-	with nested lists).
+	with nested lists). Returns: new list.
 	"""
 	result = []
 	total = 0
@@ -40,9 +42,27 @@ def cumulative_sum(l):
 		else:
 			total += x
 			result.append(total)	
-	print result		
+	return result
 
-cumulative_sum(cumulative)	
+def middle(l):
+	"""
+	Receives a list and returns a new one containing all the items
+	except the first and last one. Returns: new list.
+	"""	
+	result = []		
+	for i in range(1, len(l) - 1):
+		result.append(l[i])
+	return result
+
+def chop(l):
+	"""
+	Removes the first and last items from list l. Returns: none.
+	"""		
+	del l[len(l) - 1]
+	del l[0]
+
+
+middle(t)
 
 
 
