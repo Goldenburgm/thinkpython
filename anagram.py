b254d5fcc8b665f1b2b8d6f830ea0d85db814dd5
goldenburg <goldenburgm@gmail.com>
2016-05-30 21:23:03 -0300

    New anagram script, which prints a nested list of anagrams in given file.

---
+++ anagram.py
@@ -0,0 +1,32 @@
+def anagram(f):
+	"""f: word file (one word per line)
+	Reads given file and prints a list containing all anagrams in file,
+	in descending order.
+	"""
+	open_file = open(f)
+	anagrams_dict = dict()
+	
+	for line in open_file:
+		word = line.strip()
+		#sorting each word
+		sorted_word = "".join(sorted(word))
+		#using setdefault to initialize sorted_word 
+		#key with an empty list
+		anagrams_dict.setdefault(sorted_word, []).append(word)
+	
+	anagrams = list()
+
+	#searching for every value greater than one in dict
+	#value = number of items in list of words
+	for key, value in anagrams_dict.items():
+		if len(value) > 1:
+			anagrams.append(value)
+
+	#sorting anagram list by length, in decreasing order
+	anagrams.sort(key=len, reverse=True)
+
+	for item in anagrams:
+		print item, len(item)
+
+	
+print anagram("words.txt")		
