6a0c4c05bfd49e6db668dc40f7143cfa4959ce22
Suporte <goldenburgm@gmail.com>
2016-06-08 13:38:24 -0300

    Few ajustments on logic errors in the markov function and complete removal of the random_text() function. Now favoring if/else blocks instead of try/except blocks.

--- markov.py
+++ markov.py
@@ -1,8 +1,9 @@
+import string
 import word_frequency_analysis as wfa
 import random as r
 
 def markov_analysis(t, n):
-	"""t: word list
+        """t: word list
 	n: prefix size
 	Performs a markov analysis in given word list. Returns a dict with prefixes as keys and
 	possible suffixes as values.
@@ -10,39 +11,21 @@ def markov_analysis(t, n):
 	res = dict()
 	i = 0
 	for word in t:
-		try:
+		if i < (len(t) - n):
 			suffix = t[i+n]
-		except:
-			print "Out of range"
-		prefix = " ".join(t[i:t.index(suffix)])
+		else:
+			break
+		prefix = " ".join(t[i:(i+n)])
 		res.setdefault(prefix, []).append(suffix)
 		i += 1
 	return res
 
-def random_text(d, n=1000):
-	"""d: markov dictionary
-	n: integer
-	Prints a random text using the words in d. The text will be generated in n iterations.
-	"""
-	i = 0
-	while i < n:
-		try:
-			prefix = suffix
-			suffix = r.choice(markov_dict[prefix])
-			print prefix, suffix,
-			i += 1
-		except:
-			prefix = " ".join(r.sample(markov_dict, 1))
-			suffix = r.choice(markov_dict[prefix])
-			print prefix, suffix,
-			i += 1
-	
-book = wfa.file_to_word_list("pg41787.txt")
-markov_dict = markov_analysis(book, 2)			
-
 if __name__ == "__main__":
-	random_text(markov_dict, 300)			
+	book = wfa.file_to_word_list("pg41787.txt")
+	markov_dict = markov_analysis(book, 2)
+	for key, val in markov_test.items():
+		print key, r.choice(val),
 
-		
+
 
 
