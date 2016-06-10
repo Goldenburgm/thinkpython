0eda5445fd28c47d0d80950ddad3ffcff3b32090
Suporte <goldenburgm@gmail.com>
2016-06-10 12:56:41 -0300

    Zipf's Analysis script now allows the user to print or plot the rank/frequency relation in a given file, using matplotlib.

--- zipfs.py
+++ zipfs.py
@@ -1,5 +1,7 @@
 import string as s
 import operator as op
+import matplotlib.pyplot as pyplot
+import sys
 
 def file_word_frequency(f):
 	"""f: file
@@ -8,18 +10,40 @@ def file_word_frequency(f):
 	uppercase.
 	"""
 	t = dict()
-	open_file = open(f)
+	open_file = open(str(f))
 	for line in open_file:
 		for item in line.split():
+			#removing punctuation
 			word = "".join([char.lower() for char in item if 
 							char not in s.punctuation])
 			t[word] = t.get(word, 0) + 1
-	res = [(word, freq) for (word, freq) in t.items()]	
+	res = [(word, freq) for (word, freq) in t.items()]
+	#sorting in reverse order by the frequency in each item	
 	res.sort(reverse=True, key=op.itemgetter(1))
 	return res
 
+def main(name, file_name="pg41787.txt", flag="plot"):
+	"""file_name = name of file to plot or print results
+	flag = plot or print
+	Depending on flag, prints or plots the frequency of words in given
+	file, in descending order.
+	"""
+	#using enumerate to make a list containing (rank, frequency)
+	zipfs_list = [(item[1], rank) for rank, item in 
+				  enumerate(file_word_frequency(file_name))]
+	
+	ranks, frequencies = zip(*zipfs_list)
+
+	if flag == "plot":
+		pyplot.plot(ranks, frequencies, "r-")
+		pyplot.ylabel("Frequency")
+		pyplot.xlabel("Rank")
+		pyplot.show()
+	elif flag == "print":
+		for frequency, rank in zipfs_list:
+			print rank, frequency
+	else:
+		print "Usage: ./zipfs.py filename [print|plot]"		
 	
 if __name__ == "__main__":
-	for item in file_word_frequency("pg41787.txt"):
-		print item[0], item[1]
-	
+	main(*sys.argv)
