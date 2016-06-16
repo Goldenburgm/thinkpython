0970ced9f4be42e8d25e38233ae2d57a44adb8d3
Suporte <goldenburgm@gmail.com>
2016-06-16 17:42:54 -0300

    Optimization of the search_dir() function. Now using the glob library to use file wildcards as parameters (* and ?).

--- duplicate_files.py
+++ duplicate_files.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+
 import os
+import sys
+import glob
 
 def search_dir(dirname, file):
 	"""dirname: file path
@@ -6,19 +10,23 @@ def search_dir(dirname, file):
 	Searches the given directory path recursively and returns a list of
 	directories with files that match given file description.
 	"""
-	
 	res = list()
-
 	for root, folders, files in os.walk(dirname):
-		for name in files:
-			if name == file:
-				res.append((os.path.join(root, name)))
-	
+		path = glob.glob(str(os.path.join(root, file)))
+		for item in path:
+			res.append(item)
+	return res
+
 	#if result is empty			
 	if not res:
 		return "File '{}'' not found in directory '{}'.".format(file, dirname)
 	else:
 		return res
+	
+for item in search_dir(*sys.argv[1:])	:
+	print item
+
+
 
 	
 
