e0a391690feb25588cb01bf6c49bd2ea83bdcc89
goldenburg <goldenburgm@gmail.com>
2016-06-16 21:34:25 -0300

    New function duplicates(), that returns a list of all duplicate files of given file extension in a folder.

--- duplicate_files.py
+++ duplicate_files.py
@@ -15,16 +15,34 @@ def search_dir(dirname, file):
 		path = glob.glob(str(os.path.join(root, file)))
 		for item in path:
 			res.append(item)
-	return res
 
 	#if result is empty			
 	if not res:
 		return "File '{}'' not found in directory '{}'.".format(file, dirname)
 	else:
 		return res
+
+def duplicates(dirname, suffix):
+	"""dirname: file path
+	suffix: file extension
+	Returns a list of files with the same file extension that are 
+	essentially equal (using m5dsum) in given directory. 
+	Example: duplicates(/home/, mp3)
+	"""		
+	file_extension = "*." + suffix
+	hash_dict = dict()
+	for path in search_dir(dirname, file_extension):
+		cmd = "md5sum " + path
+		fp = os.popen(cmd)
+		hash_file = fp.read().split()
+		hash_dict.setdefault(hash_file[0], []).append(hash_file[1])
+	res = [files for hash, files in hash_dict.items() if len(files) > 1]
+	return res
 	
-for item in search_dir(*sys.argv[1:])	:
-	print item
+if __name__ == "__main__":
+	for item in duplicates(*sys.argv[1:]):
+		print item
+	
 
 
 
